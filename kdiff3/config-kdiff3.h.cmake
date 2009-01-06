0627bf66bdbbc08724556b8904bd75ad6b9e7f59
Joachim Eibl <joachim.eibl@gmx.de>
2009-01-06 18:07:30 +0000

    git-svn-id: https://kdiff3.svn.sourceforge.net/svnroot/kdiff3/trunk@86 c8aa9fed-2811-0410-8543-893ada326672

---
+++ kdiff3/config-kdiff3.h.cmake
@@ -0,0 +1,4 @@
+/* config-kdiff3.h.cmake. */
+
+/* Define to 1 if you have KDE4 libkonq shared library installed */
+#cmakedefine HAVE_LIBKONQ 1
---
+++ kdiff3/src-QT4/guiutils.h
@@ -0,0 +1,130 @@
+/***************************************************************************
+                          kdiff3.h  -  description
+                             -------------------
+    begin                : March 26 17:44 CEST 2002
+    copyright            : (c) 2008 by Valentin Rusu
+    email                : kde at rusu.info
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <qobject.h>
+#include <kactioncollection.h>
+
+namespace KDiff3 {
+
+   template <class T>
+   T* createAction( 
+	 const QString& text, 
+	 const QObject* receiver, 
+	 const char* slot, 
+	 KActionCollection* ac, 
+	 const char* actionName);
+   
+   template <>
+   inline KAction* createAction<KAction>( 
+                   const QString& text, 
+                   const QObject* receiver, 
+                   const char* slot, 
+                   KActionCollection* ac, 
+                   const char* actionName)    {
+      assert( ac != 0 );
+      KAction* theAction = ac->addAction( actionName );
+      theAction->setText( text );
+      QObject::connect( theAction, SIGNAL( triggered() ), receiver, slot );
+      return theAction;
+   }
+   template <>
+   inline KToggleAction* createAction<KToggleAction>( 
+                   const QString& text, 
+                   const QObject* receiver, 
+                   const char* slot, 
+                   KActionCollection* ac, 
+                   const char* actionName)    {
+      assert( ac != 0 );
+      KToggleAction* theAction = new KToggleAction(ac);
+      ac->addAction( actionName, theAction );
+      theAction->setText( text );
+      QObject::connect( theAction, SIGNAL( toggled(bool) ), receiver, slot );
+      return theAction;
+   }
+   
+   template <class T>
+   T* createAction( 
+	 const QString& text, 
+	 const KShortcut& shortcut, 
+	 const QObject* receiver, 
+	 const char* slot, 
+	 KActionCollection* ac, 
+	 const char* actionName) 
+   {
+      T* theAction = createAction<T>( text, receiver, slot, ac, actionName );
+      theAction->setShortcut( shortcut );
+      return theAction;
+   }
+   template <class T>
+   T* createAction(
+      const QString& text,
+      const QIcon& icon,
+      const QObject* receiver,
+      const char* slot,
+      KActionCollection* ac,
+      const char* actionName)
+   {
+      T* theAction = createAction<T>( text, receiver, slot, ac, actionName );
+      theAction->setIcon( icon );
+      return theAction;
+   }
+   template <class T>
+   T* createAction(
+      const QString& text,
+      const QIcon& icon,
+      const QString& iconText,
+      const QObject* receiver,
+      const char* slot,
+      KActionCollection* ac,
+      const char* actionName)
+   {
+      T* theAction = createAction<T>( text, receiver, slot, ac, actionName );
+      theAction->setIcon( icon );
+      theAction->setIconText( iconText );
+      return theAction;
+   }
+   template <class T>
+   T* createAction(
+	 const QString& text,
+	 const QIcon& icon,
+	 const KShortcut& shortcut,
+	 const QObject* receiver,
+	 const char* slot,
+	 KActionCollection* ac,
+	 const char* actionName)
+   {
+      T* theAction = createAction<T>( text, shortcut, receiver, slot, ac, actionName );
+      theAction->setIcon( icon );
+      return theAction;
+   }
+   template <class T>
+   T* createAction(
+         const QString& text,
+         const QIcon& icon,
+         const QString& iconText,
+         const KShortcut& shortcut,
+         const QObject* receiver,
+         const char* slot,
+         KActionCollection* ac,
+         const char* actionName)
+   {
+      T* theAction = createAction<T>( text, shortcut, receiver, slot, ac, actionName );
+      theAction->setIcon( icon );
+      theAction->setIconText( iconText );
+      return theAction;
+   }
+}
