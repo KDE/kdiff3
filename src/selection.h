18a03c7b04e7ece8b9906e71eeaa00cd9bd8c0ee
Michael Reeves <reeves.87@gmail.com>
2018-07-23 19:55:05 -0400

    Fix missing licese

--- src/selection.h
+++ src/selection.h
@@ -1,3 +1,13 @@
+/***************************************************************************
+ *   Copyright (C) 2003-2011 by Joachim Eibl                               *
+ *   joachim.eibl at gmx.de                                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
 #include "gnudiff_diff.h"
 #include "common.h"
 
