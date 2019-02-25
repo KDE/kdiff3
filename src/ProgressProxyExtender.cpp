5a8ebed6a30bba2e001fc5c5acc4f414d6405005
Michael Reeves <reeves.87@gmail.com>
2019-02-24 23:41:08 -0500

    Corrrect signture for slotPercent

--- src/ProgressProxyExtender.cpp
+++ src/ProgressProxyExtender.cpp
@@ -2,22 +2,22 @@
  * Copyright (C) 2003-2007 by Joachim Eibl
  *   joachim.eibl at gmx.de
  * Copyright (C) 2018 Michael Reeves reeves.87@gmail.com
- * 
+ *
  * This file is part of KDiff3.
- * 
+ *
  * KDiff3 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * KDiff3 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with KDiff3.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  */
 
 #include "ProgressProxyExtender.h"
@@ -29,7 +29,7 @@ void ProgressProxyExtender::slotListDirInfoMessage(KJob*, const QString& msg)
     setInformation(msg, 0);
 }
 
-void ProgressProxyExtender::slotPercent(KJob*, qint64 percent)
+void ProgressProxyExtender::slotPercent(KJob*, unsigned long percent)
 {
     setCurrent(percent);
 }
