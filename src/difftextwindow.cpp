aa37fc1736fca7000598180e97bcbf8a24412387
Michael Reeves <reeves.87@gmail.com>
2020-10-23 20:00:46 -0400

    Drop unneeded asserts

--- src/difftextwindow.cpp
+++ src/difftextwindow.cpp
@@ -1200,7 +1200,6 @@ void DiffTextWindow::print(RLPainter& p, const QRect&, int firstLine, int nofLin
 
 void DiffTextWindowData::draw(RLPainter& p, const QRect& invalidRect, int beginLine, int endLine)
 {
-    Q_ASSERT(!(m_pLineData == nullptr && m_filename != ""));
     if(m_pLineData == nullptr || m_pLineData->isEmpty()) return;
     m_lineNumberWidth = m_pOptions->m_bShowLineNumbers ? (int)log10((double)std::max(m_size, 1)) + 1 : 0;
 
@@ -1270,7 +1269,6 @@ void DiffTextWindowData::draw(RLPainter& p, const QRect& invalidRect, int beginL
 
 QString DiffTextWindowData::getString(int d3lIdx)
 {
-    Q_ASSERT(!(m_pLineData == nullptr && (m_filename != "" || m_size != 0)));
     Q_ASSERT(!(m_pLineData != nullptr && m_pLineData->isEmpty() && m_size != 0));
 
     if(m_pLineData == nullptr || m_pLineData->isEmpty() || d3lIdx < 0 || d3lIdx >= m_pDiff3LineVector->size())
