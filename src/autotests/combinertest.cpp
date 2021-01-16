448ad0d8ae8381e5a9e1780184b25be69a1cba2f
Michael Reeves <reeves.87@gmail.com>
2021-01-15 23:11:47 -0500

    Add autotest for FirstNonEmpty<T>

--- src/autotests/combinertest.cpp
+++ src/autotests/combinertest.cpp
@@ -15,6 +15,10 @@ class CombinertestTest: public QObject
     Q_OBJECT;
 
   private:
+    QString nonEmpty2(){ return "test 2";}  
+
+    QString nonEmpty1(){ return "test 1";}  
+    QString emptyString(){ return "";}
     bool yes() { return true; }
     bool no() { return false;}
 
@@ -70,6 +74,55 @@ class CombinertestTest: public QObject
         connections.push_back(test1.connect(boost::bind(&CombinertestTest::yes, this)));
 
         QVERIFY(test1());
+        connections.clear();
+    }
+
+    void testFirstNonEmpty()
+    {
+        boost::signals2::signal<QString (), FirstNonEmpty<QString>> test1;
+        std::list<boost::signals2::scoped_connection> connections;
+
+        //Just in case someone moneys with the test functions
+        QVERIFY(emptyString().isEmpty());
+        QVERIFY(!nonEmpty1().isEmpty());
+        QVERIFY(!nonEmpty1().isEmpty());
+
+        connections.clear();
+        QVERIFY(test1().isEmpty());
+
+        connections.push_back(test1.connect(boost::bind(&CombinertestTest::emptyString, this)));
+        connections.push_back(test1.connect(boost::bind(&CombinertestTest::nonEmpty1, this)));
+        connections.push_back(test1.connect(boost::bind(&CombinertestTest::nonEmpty2, this)));
+
+        QCOMPARE(test1(), nonEmpty1());
+        connections.clear();
+
+        connections.push_back(test1.connect(boost::bind(&CombinertestTest::nonEmpty1, this)));
+        connections.push_back(test1.connect(boost::bind(&CombinertestTest::emptyString, this)));
+        connections.push_back(test1.connect(boost::bind(&CombinertestTest::nonEmpty2, this)));
+        
+        QCOMPARE(test1(), nonEmpty1());
+        connections.clear();
+
+        connections.push_back(test1.connect(boost::bind(&CombinertestTest::nonEmpty1, this)));
+        connections.push_back(test1.connect(boost::bind(&CombinertestTest::emptyString, this)));
+        connections.push_back(test1.connect(boost::bind(&CombinertestTest::nonEmpty2, this)));
+        
+        QCOMPARE(test1(), nonEmpty1());
+        connections.clear();
+        
+        connections.push_back(test1.connect(boost::bind(&CombinertestTest::emptyString, this)));
+        connections.push_back(test1.connect(boost::bind(&CombinertestTest::nonEmpty2, this)));
+        connections.push_back(test1.connect(boost::bind(&CombinertestTest::nonEmpty1, this)));
+        
+        QCOMPARE(test1(), nonEmpty2());
+        connections.clear();
+
+        connections.push_back(test1.connect(boost::bind(&CombinertestTest::emptyString, this)));
+        connections.push_back(test1.connect(boost::bind(&CombinertestTest::emptyString, this)));
+        connections.push_back(test1.connect(boost::bind(&CombinertestTest::emptyString, this)));
+        
+        QCOMPARE(test1(), emptyString());
         connections.clear();
     }
 };
