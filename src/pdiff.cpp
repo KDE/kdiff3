a7acdc2137e8b9cd9701bfca142c4f930d387ed0
Michael Reeves <reeves.87@gmail.com>
2021-02-18 14:28:06 -0500

    Fix in correct file name in debug output.

--- src/pdiff.cpp
+++ src/pdiff.cpp
@@ -181,7 +181,7 @@ void KDiff3App::mainInit(TotalDiffStatus* pTotalDiffStatus, const InitFlags inFl
             if(bLoadFiles)
             {
                 pp.setInformation(i18n("Loading C"));
-                qCInfo(kdiffMain) << i18n("Loading C: %1", m_sd2->getFilename());
+                qCInfo(kdiffMain) << i18n("Loading C: %1", m_sd3->getFilename());
 
                 if(bUseCurrentEncoding)
                     m_sd3->readAndPreprocess(m_sd3->getEncoding(), false);
@@ -715,7 +715,7 @@ void KDiff3App::slotFinishMainInit()
     setUpdatesEnabled(true);
 
     bool bVisibleMergeResultWindow = !m_outputFilename.isEmpty();
-    
+
     if(m_bLoadFiles)
     {
         if(bVisibleMergeResultWindow)
