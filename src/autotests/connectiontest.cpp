2ba1a3e96b748467f4214975dc9c3eef9686a37e
Michael Reeves <reeves.87@gmail.com>
2021-05-16 14:32:02 -0400

    Remove erroneous #ifdef
    BUG:436958
    FIXED-IN:1.9.2

---
+++ src/autotests/connectiontest.cpp
@@ -0,0 +1,43 @@
+/*
+ KDiff3 - Text Diff And Merge Tool
+
+ SPDX-FileCopyrightText: 2020 Michael Reeves reeves.87@gmail.com
+ SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "../defmac.h"
+
+#include <QTest>
+#include <QtTest/qtestcase.h>
+#include <qglobal.h>
+#include <qobjectdefs.h>
+
+class SigTest: public QObject
+{
+    Q_OBJECT
+  public:
+    bool fired = false;
+   Q_SIGNALS:
+     void signal();
+
+   public Q_SLOTS:
+     void slot() { fired=true;};
+};
+
+class ConnectionTest: public QObject
+{
+    Q_OBJECT;
+  private Q_SLOTS:
+    void testConnect()
+    {
+        SigTest s;
+
+        chk_connect(&s, &SigTest::signal, &s, &SigTest::slot);
+        Q_EMIT s.signal();
+        QVERIFY(s.fired);
+    }
+};
+
+QTEST_MAIN(ConnectionTest);
+
+#include "connectiontest.moc"
