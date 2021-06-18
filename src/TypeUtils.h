83f5308869d295a25b669e5b19d729e55df21d92
Michael Reeves <reeves.87@gmail.com>
2021-06-18 15:18:43 -0400

    Explicitly include <limits> fixes build on gcc-11/clang-12.

--- src/TypeUtils.h
+++ src/TypeUtils.h
@@ -1,12 +1,13 @@
 /*
  * KDiff3 - Text Diff And Merge Tool
- * 
+ *
  * SPDX-FileCopyrightText: 2018-2020 Michael Reeves reeves.87@gmail.com
  * SPDX-License-Identifier: GPL-2.0-or-later
 */
 #ifndef TYPEUTILS_H
 #define TYPEUTILS_H
 
+#include <limits>
 #include <stdlib.h>
 #include <type_traits>
 
