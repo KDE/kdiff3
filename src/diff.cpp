KDE/kdiff3

commit 8d4fdeaf742d47942882e2453fdc28c1d0a2e2c1

Take account of user preferances when considering if white space or comments should be treated as equal.
Take account of ignore comments preferance in Diff3LineList::fineDiff when setting white space flag
Take account of ignore comments and ignoreWhite space when determining equality.

src/diff.cpp
@@ -1369,12 +1369,13 @@ void calcDiff(const QString& line1, const QString& line2, DiffList& diffList, in
     }
 }
 
-bool Diff3Line::fineDiff(bool inBTextsTotalEqual, const e_SrcSelector selector, const QVector<LineData>* v1, const QVector<LineData>* v2)
+bool Diff3Line::fineDiff(bool inBTextsTotalEqual, const e_SrcSelector selector, const QVector<LineData>* v1, const QVector<LineData>* v2, const IgnoreFlags eIgnoreFlags)
 {
     LineRef k1 = 0;
     LineRef k2 = 0;
     int maxSearchLength = 500;
-    bool bTextsTotalEqual = inBTextsTotalEqual;
+    bool bTextsTotalEqual = inBTextsTotalEqual, bIgnoreComments = eIgnoreFlags | IgnoreFlag::ignoreComments;
+    bool bIgnoreWhiteSpace = eIgnoreFlags | IgnoreFlag::ignoreWhiteSpace;
 
     Q_ASSERT(selector == e_SrcSelector::A || selector == e_SrcSelector::B || selector == e_SrcSelector::C);
 
@@ -1425,11 +1426,13 @@ bool Diff3Line::fineDiff(bool inBTextsTotalEqual, const e_SrcSelector selector, 
                     dli->setNumberOfEquals(0);
                 }
             }
-            //In case its not obvious setFineDiff moves pDiffList to a memeber variable so control changes to class scope after this call.
+
             setFineDiff(selector, pDiffList);
         }
-
-        if(((*v1)[k1].isSkipable() || (*v1)[k1].whiteLine()) && ((*v2)[k2].isSkipable() || (*v2)[k2].whiteLine()))
+        /*
+            Override default euality for white lines and comments.
+        */
+        if(((bIgnoreComments && (*v1)[k1].isSkipable()) || (bIgnoreWhiteSpace && (*v1)[k1].whiteLine())) && ((bIgnoreComments && (*v2)[k2].isSkipable()) || (bIgnoreWhiteSpace && (*v2)[k2].whiteLine())))
         {
             if(selector == e_SrcSelector::A)
             {
@@ -1490,7 +1493,7 @@ void Diff3Line::getLineInfo(const e_SrcSelector winIdx, const bool isTriple, Lin
     }
 }
 
-bool Diff3LineList::fineDiff(const e_SrcSelector selector, const QVector<LineData>* v1, const QVector<LineData>* v2)
+bool Diff3LineList::fineDiff(const e_SrcSelector selector, const QVector<LineData>* v1, const QVector<LineData>* v2, const IgnoreFlags eIgnoreFlags)
 {
     // Finetuning: Diff each line with deltas
     ProgressProxy pp;
@@ -1501,7 +1504,7 @@ bool Diff3LineList::fineDiff(const e_SrcSelector selector, const QVector<LineDat
     int listIdx = 0;
     for(i = begin(); i != end(); ++i)
     {
-        bTextsTotalEqual = i->fineDiff(bTextsTotalEqual, selector, v1, v2);
+        bTextsTotalEqual = i->fineDiff(bTextsTotalEqual, selector, v1, v2, eIgnoreFlags);
         ++listIdx;
         pp.step();
     }

src/diff.h
@@ -199,6 +199,16 @@ class DiffBufferInfo
     }
 };
 
+enum class IgnoreFlag
+{
+    none = 0,
+    ignoreWhiteSpace,
+    ignoreComments
+};
+
+Q_DECLARE_FLAGS(IgnoreFlags, IgnoreFlag);
+Q_DECLARE_OPERATORS_FOR_FLAGS(IgnoreFlags);
+
 class Diff3Line
 {
   private:
@@ -291,7 +301,7 @@ class Diff3Line
     Q_REQUIRED_RESULT inline qint32 linesNeededForDisplay() const { return mLinesNeededForDisplay; }
 
     void setLinesNeeded(const qint32 lines) { mLinesNeededForDisplay = lines; }
-    Q_REQUIRED_RESULT bool fineDiff(bool bTextsTotalEqual, const e_SrcSelector selector, const QVector<LineData>* v1, const QVector<LineData>* v2);
+    Q_REQUIRED_RESULT bool fineDiff(bool bTextsTotalEqual, const e_SrcSelector selector, const QVector<LineData>* v1, const QVector<LineData>* v2, const IgnoreFlags eIgnoreFlags);
     void mergeOneLine(e_MergeDetails& mergeDetails, bool& bConflict, bool& bLineRemoved, e_SrcSelector& src, bool bTwoInputs) const;
 
     void getLineInfo(const e_SrcSelector winIdx, const bool isTriple, LineRef& lineIdx,
@@ -322,7 +332,7 @@ class Diff3LineList: public std::list<Diff3Line>
   public:
     void findHistoryRange(const QRegularExpression& historyStart, bool bThreeFiles,
                           Diff3LineList::const_iterator& iBegin, Diff3LineList::const_iterator& iEnd, int& idxBegin, int& idxEnd) const;
-    bool fineDiff(const e_SrcSelector selector, const QVector<LineData>* v1, const QVector<LineData>* v2);
+    bool fineDiff(const e_SrcSelector selector, const QVector<LineData>* v1, const QVector<LineData>* v2, const IgnoreFlags eIgnoreFlags);
     void calcDiff3LineVector(Diff3LineVector& d3lv);
     void calcWhiteDiff3Lines(const QVector<LineData>* pldA, const QVector<LineData>* pldB, const QVector<LineData>* pldC, const bool bIgnoreComments);
 

src/options.h
@@ -73,7 +73,9 @@ public:
     [[nodiscard]] bool wordWrapOn() const { return m_bWordWrap; }
     void setWordWrap(const bool enabled) { m_bWordWrap = enabled; }
 
-    [[nodiscard]] bool ignoreComments() const { return m_bIgnoreComments;}
+    [[nodiscard]] bool ignoreComments() const { return m_bIgnoreComments; }
+
+    [[nodiscard]] bool whiteSpaceIsEqual() const { return m_bDmWhiteSpaceEqual; }
   private:
     std::list<OptionItemBase*> mOptionItemList;
 

src/pdiff.cpp
@@ -69,6 +69,13 @@ void KDiff3App::mainInit(TotalDiffStatus* pTotalDiffStatus, const InitFlags inFl
 
     bool bGUI = (inFlags & InitFlag::initGUI);
 
+    IgnoreFlags eIgnoreFlags = IgnoreFlag::none;
+    if(m_pOptions->ignoreComments())
+        eIgnoreFlags |= IgnoreFlag::ignoreComments;
+
+    if(m_pOptions->whiteSpaceIsEqual())
+        eIgnoreFlags |= IgnoreFlag::ignoreWhiteSpace;
+
     Q_ASSERT(pTotalDiffStatus != nullptr);
     //bool bPreserveCarriageReturn = m_pOptions->m_bPreserveCarriageReturn;
 
@@ -170,7 +177,7 @@ void KDiff3App::mainInit(TotalDiffStatus* pTotalDiffStatus, const InitFlags inFl
                 m_diff3LineList.calcDiff3LineListUsingAB(&m_diffList12);
                 m_diff3LineList.debugLineCheck(m_sd1->getSizeLines(), e_SrcSelector::A);
 
-                pTotalDiffStatus->setTextEqualAB(m_diff3LineList.fineDiff(e_SrcSelector::A, m_sd1->getLineDataForDisplay(), m_sd2->getLineDataForDisplay()));
+                pTotalDiffStatus->setTextEqualAB(m_diff3LineList.fineDiff(e_SrcSelector::A, m_sd1->getLineDataForDisplay(), m_sd2->getLineDataForDisplay(), eIgnoreFlags));
                 if(m_sd1->getSizeBytes() == 0) pTotalDiffStatus->setTextEqualAB(false);
 
                 pp.step();
@@ -252,35 +259,35 @@ void KDiff3App::mainInit(TotalDiffStatus* pTotalDiffStatus, const InitFlags inFl
             pp.setInformation(i18n("Linediff: A <-> B"));
             qCInfo(kdiffMain) << i18n("Linediff: A <-> B");
             if(m_sd1->hasData() && m_sd2->hasData() && m_sd1->isText() && m_sd2->isText())
-                pTotalDiffStatus->setTextEqualAB(m_diff3LineList.fineDiff(e_SrcSelector::A, m_sd1->getLineDataForDisplay(), m_sd2->getLineDataForDisplay()));
+                pTotalDiffStatus->setTextEqualAB(m_diff3LineList.fineDiff(e_SrcSelector::A, m_sd1->getLineDataForDisplay(), m_sd2->getLineDataForDisplay(), eIgnoreFlags));
             pp.step();
 
             pp.setInformation(i18n("Linediff: B <-> C"));
             qCInfo(kdiffMain) << i18n("Linediff: B <-> C");
             if(m_sd2->hasData() && m_sd3->hasData() && m_sd2->isText() && m_sd3->isText())
-                pTotalDiffStatus->setTextEqualBC(m_diff3LineList.fineDiff(e_SrcSelector::B, m_sd2->getLineDataForDisplay(), m_sd3->getLineDataForDisplay()));
+                pTotalDiffStatus->setTextEqualBC(m_diff3LineList.fineDiff(e_SrcSelector::B, m_sd2->getLineDataForDisplay(), m_sd3->getLineDataForDisplay(), eIgnoreFlags));
             pp.step();
 
             pp.setInformation(i18n("Linediff: A <-> C"));
             qCInfo(kdiffMain) << i18n("Linediff: A <-> C");
             if(m_sd1->hasData() && m_sd3->hasData() && m_sd1->isText() && m_sd3->isText())
-                pTotalDiffStatus->setTextEqualAC(m_diff3LineList.fineDiff(e_SrcSelector::C, m_sd3->getLineDataForDisplay(), m_sd1->getLineDataForDisplay()));
+                pTotalDiffStatus->setTextEqualAC(m_diff3LineList.fineDiff(e_SrcSelector::C, m_sd3->getLineDataForDisplay(), m_sd1->getLineDataForDisplay(), eIgnoreFlags));
             m_diff3LineList.debugLineCheck(m_sd2->getSizeLines(), e_SrcSelector::B);
             m_diff3LineList.debugLineCheck(m_sd3->getSizeLines(), e_SrcSelector::C);
 
             pp.setInformation(i18n("Linediff: A <-> B"));
             if(m_sd1->hasData() && m_sd2->hasData() && m_sd1->isText() && m_sd2->isText())
-                pTotalDiffStatus->setTextEqualAB(m_diff3LineList.fineDiff(e_SrcSelector::A, m_sd1->getLineDataForDisplay(), m_sd2->getLineDataForDisplay()));
+                pTotalDiffStatus->setTextEqualAB(m_diff3LineList.fineDiff(e_SrcSelector::A, m_sd1->getLineDataForDisplay(), m_sd2->getLineDataForDisplay(), eIgnoreFlags));
             pp.step();
 
             pp.setInformation(i18n("Linediff: B <-> C"));
             if(m_sd3->hasData() && m_sd2->hasData() && m_sd3->isText() && m_sd2->isText())
-                pTotalDiffStatus->setTextEqualBC(m_diff3LineList.fineDiff(e_SrcSelector::B, m_sd2->getLineDataForDisplay(), m_sd3->getLineDataForDisplay()));
+                pTotalDiffStatus->setTextEqualBC(m_diff3LineList.fineDiff(e_SrcSelector::B, m_sd2->getLineDataForDisplay(), m_sd3->getLineDataForDisplay(), eIgnoreFlags));
             pp.step();
 
             pp.setInformation(i18n("Linediff: A <-> C"));
             if(m_sd1->hasData() && m_sd3->hasData() && m_sd1->isText() && m_sd3->isText())
-                pTotalDiffStatus->setTextEqualAC(m_diff3LineList.fineDiff(e_SrcSelector::C, m_sd3->getLineDataForDisplay(), m_sd1->getLineDataForDisplay()));
+                pTotalDiffStatus->setTextEqualAC(m_diff3LineList.fineDiff(e_SrcSelector::C, m_sd3->getLineDataForDisplay(), m_sd1->getLineDataForDisplay(), eIgnoreFlags));
             pp.step();
             if(m_sd1->getSizeBytes() == 0)
             {

commit c89a6e58db62bfc95ddfbdb8b84fa26cb045d0bd

Fix wrong bit-logic operator to check set bit
Also, make it easier to read by using separate line for each flag.

Problem introduced in 8d4fdeaf742d47942882e2453fdc28c1d0a2e2c1 .

src/diff.cpp
@@ -1371,8 +1371,9 @@ bool Diff3Line::fineDiff(bool inBTextsTotalEqual, const e_SrcSelector selector, 
     LineRef k1 = 0;
     LineRef k2 = 0;
     int maxSearchLength = 500;
-    bool bTextsTotalEqual = inBTextsTotalEqual, bIgnoreComments = eIgnoreFlags | IgnoreFlag::ignoreComments;
-    bool bIgnoreWhiteSpace = eIgnoreFlags | IgnoreFlag::ignoreWhiteSpace;
+    bool bTextsTotalEqual = inBTextsTotalEqual;
+    bool bIgnoreComments = eIgnoreFlags & IgnoreFlag::ignoreComments;
+    bool bIgnoreWhiteSpace = eIgnoreFlags & IgnoreFlag::ignoreWhiteSpace;
 
     assert(selector == e_SrcSelector::A || selector == e_SrcSelector::B || selector == e_SrcSelector::C);
 