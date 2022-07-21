36102024fa10a4abdeacf4e0df9a9c9a45c9e1c9
Michael Reeves <reeves.87@gmail.com>
2022-07-21 09:18:13 -0400

    Drop 32 bit alt text.
    We are 64-bit only now support wise.

--- src/main.cpp
+++ src/main.cpp
@@ -100,10 +100,8 @@ int main(int argc, char* argv[])
     const QString i18nName = i18n("KDiff3");
     QString appVersion(KDIFF3_VERSION_STRING);
 
-    if(sizeof(void*) == 8)
-        appVersion += i18n(" (64 bit)");
-    else if(sizeof(void*) == 4)
-        appVersion += i18n(" (32 bit)");
+    appVersion += i18n(" (64 bit)");
+
     const QString description = i18n("Tool for Comparison and Merge of Files and Folders");
     const QString copyright = i18n("(c) 2002-2014 Joachim Eibl, (c) 2017 Michael Reeves KF5/Qt5 port");
     const QString homePage = QStringLiteral("https://kde.org/applications/development/kdiff3");
