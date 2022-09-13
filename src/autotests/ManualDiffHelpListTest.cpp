2d468c31ecef7f64a5ad2ee8ef10a7cb8c50bed5
Michael Reeves <reeves.87@gmail.com>
2022-09-13 16:13:15 -0400

    Add autotest for ManualDiffHelper

---
+++ src/autotests/ManualDiffHelpListTest.cpp
@@ -0,0 +1,62 @@
+#include <QObject>
+#include <QTest>
+
+#include "../diff.h"
+#include "../Logging.h"
+
+class ManualDiffHelpListTest: public QObject
+{
+    Q_OBJECT
+
+  private Q_SLOTS:
+    void testOverlappingRangesRemoved()
+    {
+        ManualDiffHelpList expected = {ManualDiffHelpEntry(e_SrcSelector::A, 20, 50)};
+        ManualDiffHelpList list = {ManualDiffHelpEntry(e_SrcSelector::A, 1, 40)};
+
+        list.insertEntry(e_SrcSelector::A, 20, 50);
+
+        QVERIFY(list.size() == 1);
+        QVERIFY(list == expected);
+    }
+
+    void testListKeeptSorted()
+    {
+        ManualDiffHelpList expected = {
+            ManualDiffHelpEntry(e_SrcSelector::A, 1, 19),
+            ManualDiffHelpEntry(e_SrcSelector::A, 20, 30),
+            ManualDiffHelpEntry(e_SrcSelector::A, 50, 60),
+        };
+        ManualDiffHelpList list;
+
+        list.insertEntry(e_SrcSelector::A, 1, 19);
+        list.insertEntry(e_SrcSelector::A, 50, 60);
+        list.insertEntry(e_SrcSelector::A, 20, 30);
+
+        QVERIFY(list.size() == 3);
+        QVERIFY(list == expected);
+    }
+
+    void testDeleteAndSort()
+    {
+        ManualDiffHelpList expected = {
+            ManualDiffHelpEntry(e_SrcSelector::A, 1, 19),
+            ManualDiffHelpEntry(e_SrcSelector::A, 25, 35),
+            ManualDiffHelpEntry(e_SrcSelector::A, 50, 60),
+        };
+        ManualDiffHelpList list;
+
+        list.insertEntry(e_SrcSelector::A, 1, 19);
+        list.insertEntry(e_SrcSelector::A, 50, 60);
+        list.insertEntry(e_SrcSelector::A, 20, 30);
+        list.insertEntry(e_SrcSelector::A, 30, 40);
+        list.insertEntry(e_SrcSelector::A, 25, 35);
+
+        QVERIFY(list.size() == 3);
+        QVERIFY(list == expected);
+    }
+};
+
+QTEST_MAIN(ManualDiffHelpListTest);
+
+#include "ManualDiffHelpListTest.moc"
