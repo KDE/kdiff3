00be1bac1b00f8e3ab2ce85608a25a57005c0a9e
Michael Reeves <reeves.87@gmail.com>
2023-05-15 20:37:06 -0400

    Don't block generation of word wrap cache
    BUG:469817
    FIXED-IN:1.10.3

--- src/difftextwindow.cpp
+++ src/difftextwindow.cpp
@@ -1679,7 +1679,7 @@ void DiffTextWindow::recalcWordWrapHelper(QtSizeType wrapLineVectorSize, int vis
         QtSizeType cacheListIdx2 = 0;
         QTextLayout textLayout(QString(), font(), this);
 
-        for(i = firstD3LineIdx; wrapLineIdx < d->m_diff3WrapLineVector.size() && i < endIdx; ++i)
+        for(i = firstD3LineIdx; i < endIdx; ++i)
         {
             if(g_pProgressDialog->wasCancelled())
                 return;
@@ -1755,6 +1755,9 @@ void DiffTextWindow::recalcWordWrapHelper(QtSizeType wrapLineVectorSize, int vis
                         d3wl.wrapLineLength = 0;
                     }
                 }
+
+                if(wrapLineIdx >= d->m_diff3WrapLineVector.size())
+                    break;
             }
         }
 
