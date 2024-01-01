f6a7934bbdccbc46e7425bb20ace3d0961026882
Michael Reeves <reeves.87@gmail.com>
2024-01-01 16:09:55 -0500

    Adjust for new KIO::listDir API

--- src/DefaultFileAccessJobHandler.cpp
+++ src/DefaultFileAccessJobHandler.cpp
@@ -428,8 +428,11 @@ bool DefaultFileAccessJobHandler::listDir(DirectoryList* pDirList, bool bRecursi
     else
     {
         KIO::ListJob* pListJob = nullptr;
+#if KF_VERSION < KF_VERSION_CHECK(5, 240, 0)
         pListJob = KIO::listDir(mFileAccess->url(), KIO::HideProgressInfo, true /*bFindHidden*/);
-
+#else
+        pListJob = KIO::listDir(mFileAccess->url(), KIO::HideProgressInfo, KIO::ListJob::ListFlags::IncludeHidden /*bFindHidden*/);
+#endif
         m_bSuccess = false;
         if(pListJob != nullptr)
         {
