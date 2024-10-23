515910b2848d8ebe47f8bdd8d05401e7830f2e0a
Michael Reeves <reeves.87@gmail.com>
2024-10-23 13:44:37 -0400

    Look in user registery then machine registry for path.

--- diff_ext_for_kdiff3/diff_ext.cpp
+++ diff_ext_for_kdiff3/diff_ext.cpp
@@ -394,7 +394,8 @@ void DIFF_EXT::diff(const tstring& arguments)
     STARTUPINFO si;
     PROCESS_INFORMATION pi;
     bool bError = true;
-    tstring command = SERVER::instance()->getRegistryKeyString(TEXT(""), TEXT("diffcommand"), false);
+    tstring command = SERVER::instance()->getRegistryKeyString(TEXT(""), TEXT("diffcommand"), true); //look in user registry first so it can be overidden
+    if(command.empty()) Scommand = SERVER::instance()->getRegistryKeyString(TEXT(""), TEXT("diffcommand"), false);
     tstring commandLine = TEXT("\"") + command + TEXT("\" ") + arguments;
     if(!command.empty())
     {
