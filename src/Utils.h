d913264f7558beb8eee2b323e2b2adee6cf1a705
Michael Reeves <reeves.87@gmail.com>
2024-11-30 10:47:56 -0500

    Fix formatting

--- src/Utils.h
+++ src/Utils.h
@@ -18,26 +18,27 @@
 #include <QStringList>
 #include <QUrl>
 
-class Utils{
+class Utils
+{
   public:
-      // QStringConverter::availableCodecs() is too broad for our needs.
-      static QStringList availableCodecs();
+    // QStringConverter::availableCodecs() is too broad for our needs.
+    static QStringList availableCodecs();
 
-      static bool isLocalFile(const QUrl &url);
-      /*
-        QUrl::toLocalFile does some special handling for locally visable windows network drives.
-        However if QUrl::isLocal returns false we get an empty string back.
-      */
-      static QString urlToString(const QUrl &url);
-      static bool wildcardMultiMatch(const QString& wildcard, const QString& testString, bool bCaseSensitive);
-      static QString getArguments(QString cmd, QString& program, QStringList& args);
-      static bool isEndOfLine(QChar c) { return c == '\n'; } //interally all line endings are converted to '\n'
+    static bool isLocalFile(const QUrl& url);
+    /*
+      QUrl::toLocalFile does some special handling for locally visable windows network drives.
+      However if QUrl::isLocal returns false we get an empty string back.
+    */
+    static QString urlToString(const QUrl& url);
+    static bool wildcardMultiMatch(const QString& wildcard, const QString& testString, bool bCaseSensitive);
+    static QString getArguments(QString cmd, QString& program, QStringList& args);
+    static bool isEndOfLine(QChar c) { return c == '\n'; } //interally all line endings are converted to '\n'
 
-      static void calcTokenPos(const QString& s, qint32 posOnScreen, qsizetype& pos1, qsizetype& pos2);
-      static QString calcHistoryLead(const QString& s);
+    static void calcTokenPos(const QString& s, qint32 posOnScreen, qsizetype& pos1, qsizetype& pos2);
+    static QString calcHistoryLead(const QString& s);
 
-    private:
-      static bool isCTokenChar(QChar c);
+  private:
+    static bool isCTokenChar(QChar c);
 };
 
 #endif
