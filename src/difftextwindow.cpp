KDE/kdiff3

commit 61f9301d3d41e6307847de641ef058f34d0df96c

Pass SourceData object as parameter for DiffTextWindow::init

src/difftextwindow.cpp
@@ -138,23 +138,21 @@ class DiffTextWindowData
     }
 
     void init(
-        const QString& filename,
-        const char* encoding,
-        e_LineEndStyle eLineEndStyle,
-        const std::shared_ptr<LineDataVector>& pLineData,
-        LineType size,
+        const std::shared_ptr<SourceData> sd,
         const Diff3LineVector* pDiff3LineVector,
         const ManualDiffHelpList* pManualDiffHelpList)
     {
         reset();
-        m_filename = filename;
-        m_pLineData = pLineData;
-        m_size = size;
+
+        mSourceData = sd;
+        m_filename = sd->getAliasName();
+        m_pLineData = sd->getLineDataForDisplay();
+        m_size = sd->getSizeLines();
         mDiff3LineVector = pDiff3LineVector;
         m_pManualDiffHelpList = pManualDiffHelpList;
 
-        mTextEncoding = QString::fromLatin1(encoding);
-        m_eLineEndStyle = eLineEndStyle;
+        mTextEncoding = QString::fromLatin1(sd->getEncoding());
+        m_eLineEndStyle = sd->getLineEndStyle();
     }
 
     void reset()
@@ -241,6 +239,7 @@ class DiffTextWindowData
     QString mTextEncoding;
     e_LineEndStyle m_eLineEndStyle;
 
+    std::shared_ptr<SourceData> mSourceData;
     std::shared_ptr<LineDataVector> m_pLineData;
     LineType m_size = 0;
     QString m_filename;
@@ -338,7 +337,7 @@ DiffTextWindow::DiffTextWindow(DiffTextWindowFrame* pParent,
     setAcceptDrops(true);
     mWinIdx = winIdx;
 
-    init(QString(""), nullptr, d->m_eLineEndStyle, nullptr, 0, nullptr, nullptr);
+    init(d->mSourceData, nullptr, nullptr);
 
     setMinimumSize(QSize(20, 20));
 
@@ -350,15 +349,12 @@ DiffTextWindow::DiffTextWindow(DiffTextWindowFrame* pParent,
 DiffTextWindow::~DiffTextWindow() = default;
 
 void DiffTextWindow::init(
-    const QString& filename,
-    const char* encoding,
-    e_LineEndStyle eLineEndStyle,
-    const std::shared_ptr<LineDataVector>& pLineData,
-    LineType size,
+    const std::shared_ptr<SourceData> sd,
+
     const Diff3LineVector* pDiff3LineVector,
     const ManualDiffHelpList* pManualDiffHelpList)
 {
-    d->init(filename, encoding, eLineEndStyle, pLineData, size, pDiff3LineVector, pManualDiffHelpList);
+    d->init(sd, pDiff3LineVector, pManualDiffHelpList);
 
     update();
 }

src/difftextwindow.h
@@ -55,11 +55,7 @@ class DiffTextWindow: public QWidget
     DiffTextWindow(DiffTextWindowFrame* pParent, e_SrcSelector winIdx, KDiff3App& app);
     ~DiffTextWindow() override;
     void init(
-        const QString& fileName,
-        const char* pTextCodec,
-        e_LineEndStyle eLineEndStyle,
-        const std::shared_ptr<LineDataVector>& pLineData,
-        LineType size,
+        const std::shared_ptr<SourceData> sd,
         const Diff3LineVector* pDiff3LineVector,
         const ManualDiffHelpList* pManualDiffHelpList);
 

src/pdiff.cpp
@@ -369,16 +369,13 @@ void KDiff3App::mainInit(TotalDiffStatus* pTotalDiffStatus, const InitFlags inFl
     if(bGUI)
     {
         const ManualDiffHelpList* pMDHL = &m_manualDiffHelpList;
-        m_pDiffTextWindow1->init(m_sd1->getAliasName(), m_sd1->getEncoding(), m_sd1->getLineEndStyle(),
-                                 m_sd1->getLineDataForDisplay(), m_sd1->getSizeLines(), &mDiff3LineVector, pMDHL);
+        m_pDiffTextWindow1->init(m_sd1, &mDiff3LineVector, pMDHL);
         m_pDiffTextWindowFrame1->init();
 
-        m_pDiffTextWindow2->init(m_sd2->getAliasName(), m_sd2->getEncoding(), m_sd2->getLineEndStyle(),
-                                 m_sd2->getLineDataForDisplay(), m_sd2->getSizeLines(), &mDiff3LineVector, pMDHL);
+        m_pDiffTextWindow2->init(m_sd2, &mDiff3LineVector, pMDHL);
         m_pDiffTextWindowFrame2->init();
 
-        m_pDiffTextWindow3->init(m_sd3->getAliasName(), m_sd3->getEncoding(), m_sd3->getLineEndStyle(),
-                                 m_sd3->getLineDataForDisplay(), m_sd3->getSizeLines(), &mDiff3LineVector, pMDHL);
+        m_pDiffTextWindow3->init(m_sd3, &mDiff3LineVector, pMDHL);
         m_pDiffTextWindowFrame3->init();
 
         m_pDiffTextWindowFrame3->setVisible(m_bTripleDiff);
@@ -1450,19 +1447,19 @@ bool KDiff3App::doDirectoryCompare(const bool bCreateNewInstance)
             m_sd1->reset();
             if(m_pDiffTextWindow1 != nullptr)
             {
-                m_pDiffTextWindow1->init(QString(""), nullptr, eLineEndStyleDos, nullptr, 0, nullptr, nullptr);
+                m_pDiffTextWindow1->init(m_sd1, nullptr, nullptr);
                 m_pDiffTextWindowFrame1->init();
             }
             m_sd2->reset();
             if(m_pDiffTextWindow2 != nullptr)
             {
-                m_pDiffTextWindow2->init(QString(""), nullptr, eLineEndStyleDos, nullptr, 0, nullptr, nullptr);
+                m_pDiffTextWindow2->init(m_sd2, nullptr, nullptr);
                 m_pDiffTextWindowFrame2->init();
             }
             m_sd3->reset();
             if(m_pDiffTextWindow3 != nullptr)
             {
-                m_pDiffTextWindow3->init(QString(""), nullptr, eLineEndStyleDos, nullptr, 0, nullptr, nullptr);
+                m_pDiffTextWindow3->init(m_sd3, nullptr, nullptr);
                 m_pDiffTextWindowFrame3->init();
             }
         }

commit 1ff9be019f9670efddb174086b72894d3423ff1f

Fix crash on startup
regression from 61f9301d3d41e6307847de641ef058f34d0df96c and

src/difftextwindow.cpp
@@ -139,6 +139,8 @@ class DiffTextWindowData
     {
         reset();
 
+        assert(sd != nullptr);
+
         mSourceData = sd;
         m_pLineData = mSourceData->getLineDataForDisplay();
         mDiff3LineVector = pDiff3LineVector;
@@ -147,6 +149,7 @@ class DiffTextWindowData
 
     void reset()
     {
+        mSourceData.reset();
         //wait for all helper threads to finish
         while(DiffTextWindow::maxThreads() > 0) {} //Clear word wrap threads.
 
@@ -225,7 +228,7 @@ class DiffTextWindowData
 
     QPointer<DiffTextWindow> m_pDiffTextWindow;
 
-    std::shared_ptr<SourceData> mSourceData;
+    std::shared_ptr<SourceData> mSourceData = std::make_shared<SourceData>(); //Empty data for early init.
     std::shared_ptr<LineDataVector> m_pLineData;
     bool m_bWordWrap = false;
     qint32 m_delayedDrawTimer = 0;
@@ -321,6 +324,7 @@ DiffTextWindow::DiffTextWindow(DiffTextWindowFrame* pParent,
     setAcceptDrops(true);
     mWinIdx = winIdx;
 
+    assert(d->mSourceData != nullptr);
     init(d->mSourceData, nullptr, nullptr);
 
     setMinimumSize(QSize(20, 20));